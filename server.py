a1ab3fe4c2d41fe08ca1d7d2a768ae9cea6d609b
sarenkov <sarenkov@skbkontur.ru>
2020-05-17 14:03:34 +0500

    Edit server.py 2

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 import sentry_sdk
 from bottle import Bottle, run, error, response
 from sentry_sdk.integrations.bottle import BottleIntegration
+import os
 
 # Настройка Sentry
 sentry_sdk.init(
@@ -11,24 +12,29 @@ sentry_sdk.init(
 
 app = Bottle()
 
+
 def get_response_text(status):
     return f'<h1>Текущий статус: {status}. Можно проверить в консоли</h1>'
+
 
 @app.route('/success')
 def success():
     response.status = 200
     return get_response_text(response.status)
 
+
 @app.route('/fail')
 def fail():
-    print('hello')    
-    raise Exception("Статус ответа 500. Сервис ходит по лезвию просто. Ошибка отправлена в Sentry")    
+    print('hello')
+    raise Exception(
+        "Статус ответа 500. Сервис ходит по лезвию просто. Ошибка отправлена в Sentry")
 
 
 @app.route('/notFound')
 def not_found():
     response.status = 404
     return get_response_text(response.status)
+
 
 @app.route('/unautorized')
 def unautorized():
